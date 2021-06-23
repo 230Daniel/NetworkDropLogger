610e372c18dab91ea8247206bd8cceb891dc6b31
Daniel Baynton <daniel.baynton@hotmail.com>
2021-06-23 20:53:35 +0100

    Initial commit

---
+++ NetworkDropLogger/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetworkDropLogger
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
